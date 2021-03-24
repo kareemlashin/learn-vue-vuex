5cacd65c4feb0e545a045f1859967a828d6ad709
kareemlashin <kareemelsayed887@gmail.com>
2021-03-24 10:32:29 +0200

    validation

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -10,39 +10,91 @@
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
+          @keyup="valid()"
+
         />
       </div>
+      <div >
+        <div v-if="$v.form.id.$error">
+          <div  v-if="!$v.form.id.required" class="alert alert-danger" role="alert">
+            Field is required
+          
+          </div>
+      </div>
+    </div>
+
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
+        <label for="exampleInputPassword1">title</label>
         <input
           type="text"
     v-model="form.title"
           class="form-control"
           id="exampleInputPassword1"
+          @keyup="valid()"
         />
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <div v-if="$v.form.title.$error">
+        <div  v-if="!$v.form.title.required" class="alert alert-danger" role="alert">
+          Field is required
+        
+        </div>
+      </div>
+     
+      <button type="submit" :disabled="submitStatus" class="btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators'
+
 export default {
   name: "Add",
   created() {},
   data() {
     return {
+      submitStatus:true,
       form: {
         id: "",
         title: "",
       },
     };
   },
+  validations: {
+    form:{  id: {
+      required,
+
+    },
+    title: {
+      required
+    }}
+  },
   props: {},
   methods: {
+    valid(){
+      this.$v.form.$touch()
+
+      if (this.$v.form.$invalid) {
+        this.submitStatus = true;
+
+      } else {
+        this.submitStatus = false;
+
+      }
+    },
     onSubmit() {
+      this.$v.form.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = true
+      } else {
+        this.submitStatus = false;
+        this.$v.form.$reset;
+        this.$v.form.id.$reset;
+
       this.$store.dispatch("addUsers",{title:this.form.title,userId: this.form.id
 });
+
+}
     },
   },
 };
